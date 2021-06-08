dce10ea65735eb8bc7842e2f046d1f3a02a62661
Robert <Robromano2@gmail.com>
2021-06-07 23:12:58 -0400

    Set up state and add form to create exercises

--- frontend/src/components/CreateExercise.jsx
+++ frontend/src/components/CreateExercise.jsx
@@ -1,9 +1,101 @@
-const CreateExercise = () => {
-	return (
-		<div>
-			<p>You are on the Create Exercise component</p>
-		</div>
-	)
-}
+import { useState } from "react";
 
-export default CreateExercise
+const CreateExercise = () => {
+  const [username, setUsername] = useState("");
+  const [description, setDescription] = useState("");
+  const [duration, setDuration] = useState(0);
+  const [date, setDate] = useState(new Date());
+  const [users, setUsers] = useState([]);
+
+  // const [exercise, setExercise] = useState({
+  //   username: "",
+  //   description: "",
+  //   duration: 0,
+  //   date: new Date(),
+  // });
+
+  useEffect(() => {
+    setUsers([...users, "test user"]);
+    setUsername("test user");
+  }, []);
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const onChangeDuration = (e) => {
+    setDuration(e.target.value);
+  };
+
+  const onChangeDate = (date) => {
+    setDate(date);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const exercise = {
+      username,
+      description,
+      duration,
+      date,
+    };
+
+    console.log(exercise);
+    window.location = "/";
+  };
+
+  return (
+    <div>
+      <h3>Create New Exercise Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            ref="userInput"
+            required
+            className="form-control"
+            value={username}
+            onChange={onChangeUsername}
+          >
+            {users.map((user) => {
+              return (
+                <option key={user} value={user}>
+                  {user}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+            type="text"
+            className="form-control"
+            value={duration}
+            onChange={onChangeDuration}
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker selected={date} onChange={onChangeDate} />
+          </div>
+        </div>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create Exercise Log"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default CreateExercise;
